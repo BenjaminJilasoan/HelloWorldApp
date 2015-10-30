156fd89dd3415f6d131e9e2d71def830662a47cb
Benjamin Lykins <benjamin@jilasoan.com>
2015-10-30 16:09:34 +0000

    This launches. This is for a backup

--- Hello World App/scripts/app.js
+++ Hello World App/scripts/app.js
@@ -1,4 +1,3 @@
-
 (function () {
 
     // store a reference to the application object that will be created
@@ -7,44 +6,57 @@
 
     // create an object to store the models for each view
     window.APP = {
-      models: {
-        home: {
-          title: 'Home'
+        models: {
+            home: {
+                title: 'Home',
+            },
+            settings: {
+                title: 'Settings'
+            },
+            contacts: {
+                title: 'Contacts',
+                ds: new kendo.data.DataSource({
+                    data: [{
+                        id: 1,
+                        name: 'Bob'
+                    }, {
+                        id: 2,
+                        name: 'Mary'
+                    }, {
+                        id: 3,
+                        name: 'John'
+                    }]
+                }),
+                alert: function (e) {
+                    alert(e.data.name);
+                }
+            }
         },
-        settings: {
-          title: 'Settings'
-        },
-        contacts: {
-          title: 'Contacts',
-          ds: new kendo.data.DataSource({
-            data: [{ id: 1, name: 'Bob' }, { id: 2, name: 'Mary' }, { id: 3, name: 'John' }]
-          }),
-          alert: function(e) {
-            alert(e.data.name);
-          }
+        star: function () {
+            app.navigate("views/settings.html");
         }
-      }
     };
 
     // this function is called by Cordova when the application is loaded by the device
-    document.addEventListener('deviceready', function () {  
-      
-      // hide the splash screen as soon as the app is ready. otherwise
-      // Cordova will wait 5 very long seconds to do it for you.
-      navigator.splashscreen.hide();
+    document.addEventListener('deviceready', function () {
 
-      app = new kendo.mobile.Application(document.body, {
-        
-        // you can change the default transition (slide, zoom or fade)
-        transition: 'slide',
-        
-        // comment out the following line to get a UI which matches the look
-        // and feel of the operating system
-        skin: 'flat',
+        // hide the splash screen as soon as the app is ready. otherwise
+        // Cordova will wait 5 very long seconds to do it for you.
+        navigator.splashscreen.hide();
 
-        // the application needs to know which view to load first
-        initial: 'views/home.html'
-      });
+        app = new kendo.mobile.Application(document.body, {
+
+            // you can change the default transition (slide, zoom or fade)
+            transition: 'slide',
+
+            // comment out the following line to get a UI which matches the look
+            // and feel of the operating system
+            skin: 'flat',
+
+            // the application needs to know which view to load first
+            initial: 'views/home.html',
+            useNativeScrolling: true,
+        });
 
     }, false);
 
